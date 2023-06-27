9d022e116f163a8ec7d9674d968baeca5001ac03
chimevictory <chimevictory1@gmail.com>
2023-06-27 04:20:03 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,11 +12,8 @@
  */
 
 int main(void)
-
 {
-
 	int pass[100];
-
 	int i, sum, n;
 
 	sum = 0;
@@ -24,23 +21,19 @@ int main(void)
 	srand(time(NULL));
 
 	for (i = 0; i < 100; i++)
-
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 		if ((2772 - sum) - '0' < 78)
-
 		{
 			n = 2772 - sum - '0;
 			sum += n;
 			putchar(n + '0');
-
 			break;
 		}
 
 	}
 
 	return (0);
-
 }
